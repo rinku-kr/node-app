4ff5634149a4ce7b890c07ade3de2753445a8bd5
rinku-kr <rk24736599@gmail.com>
2024-01-08 09:20:09 +0530

    Converting html file to pug!

--- routes/admin.js
+++ routes/admin.js
@@ -9,12 +9,11 @@ const router = express.Router();
 const products = [];
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product.pug", { pageTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  // console.log(req.body);
   products.push({ title: req.body.title });
   res.redirect("/");
 });
